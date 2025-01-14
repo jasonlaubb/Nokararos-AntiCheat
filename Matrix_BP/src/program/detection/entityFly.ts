a9ccf124fd7f2f9e778d705beee1f4a59310a15e
jasonlaubb <jasonlau5378@gmai.com>
2025-01-14 21:24:50 +0800

    uwu

--- Matrix_BP/src/program/detection/entityFly.ts
+++ Matrix_BP/src/program/detection/entityFly.ts
@@ -1,7 +1,7 @@
 import { Player, Vector3 } from "@minecraft/server";
 import { IntegratedSystemEvent, Module } from "../../matrixAPI";
 import { rawtextTranslate } from "../../util/rawtext";
-import { fastAbs } from "../../util/fastmath";
+import { fastAbs, fastBelow } from "../../util/fastmath";
 import { isSurroundedByAir } from "../../util/util";
 let runId: IntegratedSystemEvent;
 const entityFly = new Module()
@@ -21,18 +21,25 @@ const entityFly = new Module()
 			pastVelocityY: new Array(10).fill(0),
 			lastNotRidingLocation: player.location,
 			prefectCombo: 0,
+			superCombo: 0,
 		});
+	})
+	.initClear((playerId) => {
+		entityFlyData.delete(playerId);
 	});
 entityFly.register();
 interface EntityFlyData {
 	pastVelocityY: number[];
 	lastNotRidingLocation: Vector3;
 	prefectCombo: number;
+	superCombo: number;
 }
 const entityFlyData = new Map<string, EntityFlyData>();
 const SPEED_THRESHOLD = 0.35;
 const LOWEST_Y_THRESHOLD = 0.4;
 const MIN_COMBO_BEFORE_FLAG = 10;
+const NORMAL_SPEED = 0.25;
+const MIN_SUPER_COMBO = 20;
 function tickEvent (player: Player) {
 	player.onScreenDisplay.setActionBar(`xz Vel: ${Math.sqrt(player.getVelocity().x **2 + player.getVelocity().z ** 2).toFixed(3)} |\n y Vel: ${player.getVelocity().y.toFixed(3)} | isOnGround: ${player.isOnGround} | yLoc: ${player.location.y.toFixed(3)}`);
 	const isRiding = player.getComponent("riding")?.entityRidingOn;
@@ -43,14 +50,18 @@ function tickEvent (player: Player) {
 	if (!isRiding) {
 		data.lastNotRidingLocation = player.location;
 		data.prefectCombo = 0;
-	} else if (isRiding.typeId.startsWith("minecraft:")) {
-		if (fastAbs(velocityY) > LOWEST_Y_THRESHOLD && player.isOnGround) {
+	} else if (isRiding.typeId.startsWith("minecraft:") && player.isOnGround) {
+		if (fastAbs(velocityY) > LOWEST_Y_THRESHOLD) {
+			let isEntityFly = false;
 			for (let i = 1; i < data.pastVelocityY.length; i++) {
 				if (data.pastVelocityY[i] !== data.pastVelocityY[i - 1]) {
-					player.teleport(data.lastNotRidingLocation);
-					player.flag(entityFly, { t: "1", velocityY });
+					isEntityFly = false;
 					break;
 				}
+			}
+			if (isEntityFly) {
+				player.teleport(data.lastNotRidingLocation);
+				player.flag(entityFly, { t: "1", velocityY });
 			}
 		}
 		const horizontalSpeed = Math.sqrt(x ** 2 + z ** 2);
@@ -62,6 +73,21 @@ function tickEvent (player: Player) {
 				data.prefectCombo = 0;
 			}
 		} else data.prefectCombo = 0;
+		if (isRiding.typeId.includes("boat") && horizontalSpeed > NORMAL_SPEED && player.isOnGround) {
+			const blockBelow = fastBelow(isRiding.location, isRiding.dimension);
+			if (blockBelow) {
+				const isAllNonIce = blockBelow.every((block) => block ? block.typeId?.includes("ice") : true);
+				const atLeastOneSolid = blockBelow.some((block) => block ? block.isSolid : false);
+				if (isAllNonIce && atLeastOneSolid) {
+					data.superCombo++;
+					if (data.superCombo >= MIN_SUPER_COMBO) {
+						player.teleport(data.lastNotRidingLocation);
+						player.flag(entityFly, { t: "3", horizontalSpeed });
+						data.superCombo = 0;
+					} else if (data.superCombo > 0) data.superCombo -= 1;
+				} else if (data.superCombo > 0) data.superCombo -= 1;
+			} else data.superCombo = 0;
+		}
 	}
 	entityFlyData.set(player.id, data);
 }
