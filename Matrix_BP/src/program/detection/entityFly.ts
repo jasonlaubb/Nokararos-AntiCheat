f6bf02fba4f7bc47cadbac160f01de3dc6db60d8
jasonlaubb <jasonlau5378@gmai.com>
2025-01-14 22:13:15 +0800

    uwu

--- Matrix_BP/src/program/detection/entityFly.ts
+++ Matrix_BP/src/program/detection/entityFly.ts
@@ -18,7 +18,7 @@ const entityFly = new Module()
 	})
 	.initPlayer((playerId, player) => {
 		entityFlyData.set(playerId, {
-			pastVelocityY: new Array(10).fill(0),
+			pastVelocityY: new Array(5).fill(0),
 			lastNotRidingLocation: player.location,
 			prefectCombo: 0,
 			superCombo: 0,
@@ -42,8 +42,8 @@ const LOWEST_Y_THRESHOLD = 0.25;
 const MIN_COMBO_BEFORE_FLAG = 10;
 const NORMAL_SPEED = 0.25;
 const MIN_SUPER_COMBO = 20;
-const FACTOR = 2;
 const FACTOR_MIN_FLAG_AMOUNT = 4;
+const FACTOR = 0.01;
 function tickEvent (player: Player) {
 	const isRiding = player.getComponent("riding")?.entityRidingOn;
 	const data = entityFlyData.get(player.id)!;
@@ -79,24 +79,21 @@ function tickEvent (player: Player) {
 		if (isRiding.typeId.includes("boat") && horizontalSpeed > NORMAL_SPEED) {
 			const stringPoint = player.location.y.toFixed(3);
 			const isOnGround = stringPoint.endsWith(".225") || stringPoint.endsWith(".725");
-			player.onScreenDisplay.setActionBar(`${horizontalSpeed.toFixed(2)} | ${isRiding.typeId} | ${stringPoint} | ${velocityY.toPrecision(FACTOR)}`);
 			if (isOnGround) {
 				const actualLocation = { x: player.location.x, y: player.location.y + 0.225, z: player.location.z };
 				const blockBelow = fastBelow(actualLocation, isRiding.dimension);
-				const isAllNonIce = blockBelow ? blockBelow.every((block) => block ? block.typeId?.includes("ice") : true) : false;
-				
+				const isAllNonIce = blockBelow ? blockBelow.every((block) => block ? !block.typeId?.includes("ice") : true) : false;
 				if (isAllNonIce && isOnGround) {
 					data.superCombo++;
-					player.sendMessage(`${data.superCombo} | ${horizontalSpeed.toFixed(2)}`);
 					if (data.superCombo >= MIN_SUPER_COMBO) {
 						player.teleport(data.lastNotRidingLocation);
 						player.flag(entityFly, { t: "3", horizontalSpeed });
 						data.superCombo = 0;
-					} else if (data.superCombo > 0) data.superCombo -= 1;
+					}
 				} else if (data.superCombo > 0) data.superCombo -= 1;
 			} else if (data.superCombo > 0) data.superCombo -= 1;
 		} else data.superCombo = 0;
-		if (velocityY >= 0.05 && isRiding.typeId !== MinecraftEntityTypes.Minecart && (velocityY.toFixed(2) === velocityY.toString() || velocityY.toFixed(1) === velocityY.toString())) {
+		if (isRiding.typeId !== MinecraftEntityTypes.Minecart && velocityY % FACTOR === 0) {
 			data.illegalFactorAmount++;
 			if (data.illegalFactorAmount >= FACTOR_MIN_FLAG_AMOUNT) {
 				player.teleport(data.lastNotRidingLocation);
