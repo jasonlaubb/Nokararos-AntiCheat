81578c7ba724db42490d16931d6c00e41740183b
jasonlaubb <jasonlau5378@gmai.com>
2025-01-14 21:54:16 +0800

    uwu

--- Matrix_BP/src/program/detection/entityFly.ts
+++ Matrix_BP/src/program/detection/entityFly.ts
@@ -2,6 +2,7 @@ import { Player, Vector3 } from "@minecraft/server";
 import { IntegratedSystemEvent, Module } from "../../matrixAPI";
 import { rawtextTranslate } from "../../util/rawtext";
 import { fastAbs, fastBelow } from "../../util/fastmath";
+import { MinecraftEntityTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
 let runId: IntegratedSystemEvent;
 const entityFly = new Module()
 	.addCategory("detection")
@@ -21,6 +22,7 @@ const entityFly = new Module()
 			lastNotRidingLocation: player.location,
 			prefectCombo: 0,
 			superCombo: 0,
+			illegalFactorAmount: 0,
 		});
 	})
 	.initClear((playerId) => {
@@ -32,6 +34,7 @@ interface EntityFlyData {
 	lastNotRidingLocation: Vector3;
 	prefectCombo: number;
 	superCombo: number;
+	illegalFactorAmount: number;
 }
 const entityFlyData = new Map<string, EntityFlyData>();
 const SPEED_THRESHOLD = 0.35;
@@ -39,8 +42,9 @@ const LOWEST_Y_THRESHOLD = 0.4;
 const MIN_COMBO_BEFORE_FLAG = 10;
 const NORMAL_SPEED = 0.25;
 const MIN_SUPER_COMBO = 20;
+const FACTOR = 2;
+const FACTOR_MIN_FLAG_AMOUNT = 4;
 function tickEvent (player: Player) {
-	player.onScreenDisplay.setActionBar(`xz Vel: ${Math.sqrt(player.getVelocity().x **2 + player.getVelocity().z ** 2).toFixed(3)} |\n y Vel: ${player.getVelocity().y.toFixed(3)} | isOnGround: ${player.isOnGround} | yLoc: ${player.location.y.toFixed(3)}`);
 	const isRiding = player.getComponent("riding")?.entityRidingOn;
 	const data = entityFlyData.get(player.id)!;
 	const { x, y: velocityY, z } = player.getVelocity();
@@ -72,6 +76,7 @@ function tickEvent (player: Player) {
 				data.prefectCombo = 0;
 			}
 		} else data.prefectCombo = 0;
+		player.onScreenDisplay.setActionBar(`${horizontalSpeed.toFixed(2)} | ${isRiding.typeId}`);
 		if (isRiding.typeId.includes("boat") && horizontalSpeed > NORMAL_SPEED) {
 			const stringPoint = player.location.y.toFixed(3);
 			const isOnGround = stringPoint.endsWith(".225") || stringPoint.endsWith(".725");
@@ -79,9 +84,10 @@ function tickEvent (player: Player) {
 				const actualLocation = { x: player.location.x, y: player.location.y + 0.225, z: player.location.z };
 				const blockBelow = fastBelow(actualLocation, isRiding.dimension);
 				const isAllNonIce = blockBelow ? blockBelow.every((block) => block ? block.typeId?.includes("ice") : true) : false;
+				
 				if (isAllNonIce && isOnGround) {
 					data.superCombo++;
-					player.sendMessage(`${data.superCombo}`);
+					player.sendMessage(`${data.superCombo} | ${horizontalSpeed.toFixed(2)}`);
 					if (data.superCombo >= MIN_SUPER_COMBO) {
 						player.teleport(data.lastNotRidingLocation);
 						player.flag(entityFly, { t: "3", horizontalSpeed });
@@ -90,6 +96,14 @@ function tickEvent (player: Player) {
 				} else if (data.superCombo > 0) data.superCombo -= 1;
 			} else if (data.superCombo > 0) data.superCombo -= 1;
 		} else data.superCombo = 0;
+		if (isRiding.typeId !== MinecraftEntityTypes.Minecart && velocityY.toPrecision(FACTOR) === velocityY.toString()) {
+			data.illegalFactorAmount++;
+			if (data.illegalFactorAmount >= FACTOR_MIN_FLAG_AMOUNT) {
+				player.teleport(data.lastNotRidingLocation);
+				player.flag(entityFly, { t: "4", velocityY });
+				data.illegalFactorAmount = 0;
+			}
+		} else data.illegalFactorAmount = 0;
 	}
 	entityFlyData.set(player.id, data);
 }
